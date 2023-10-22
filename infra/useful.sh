395a9ef5ba63316ae102c7a9a196b0990e22840b
aish <aishwaryaprabhat@gmail.com>
2023-10-22 05:29:14 +0000

    integrations working e2e

--- infra/useful.sh
+++ infra/useful.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d && echo
-kubectl -n argowf exec -it argowf-argo-workflows-server-cc64696d5-dlj8m argo auth token
+kubectl -n argowf exec -it $(kubectl get pods -n argowf | grep argo-workflows-server | awk '{print $1}') -- argo auth token
 
 # Kill prior services running on the ports
 for port in 9090 8080 9093 8501 5000 2746; do
